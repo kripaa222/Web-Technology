6a89a073d92bb032e081fb9e0ab9775c9e4f42c7
kripaa222 <NP03CS4S230272@heraldcollege.edu.np>
2023-03-31 13:11:35 +0545

    Added all the files till week 6

---
+++ workshop.py
@@ -0,0 +1,12 @@
+city=(input("Enter the city you want: "))
+if city=="Kathamandu" :
+    print("Pashupatinath temple")
+elif city=="Pokhara" :
+    print("Fewa lake")
+elif city=="Nepalgunj"
+    print("Bageshwori temple")
+elif city=="Birgunj" :
+    print("Birgunj Ghanta Ghar")
+else:
+    print("error city not found in the pycharm")
+    
